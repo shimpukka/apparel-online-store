69f54776ba282df05a50c0dcdc575bbeb6210983
Shiori Sasaki <shioris.sk@gmail.com>
2020-09-04 12:22:21 +0300

    added session persistence with reduxt persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,23 @@
 // root reducer is the base reducer that combines all the states together
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+// this storage = local storage of the window object of the browser, telling redux to use local storage as the default storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
